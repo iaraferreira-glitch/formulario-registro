50bca8c39a0130ed194e7abdd91400f19fd464e8
teiu1234 <denisezorrilla18@gmail.com>
2025-08-15 09:46:21 -0300

    nueva validacion de contraseña, no menos q 6

--- registro/js/validacion.js
+++ registro/js/validacion.js
@@ -6,12 +6,16 @@ function showAlertError() {
   document.getElementById("alert-danger").classList.add("show");
 }
 
+
+document.getElementById("regBtn").addEventListener ("click", function() {
 function showAlertError() { const password = document.getElementById("password").value; 
 if (password.length < 6) { document.getElementById("alert-danger").classList.add("show");
 
  } else { document.getElementById("alert-danger").classList.remove("show"); 
   } 
 }
+ }); 
+
 
 
 //Acá empieza la validación de que ningún campo esté vacío y de contraseña, uní ambas para que no se repita el addEventListener
