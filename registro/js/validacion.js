81af8aab05122f9d31282889c58e7ae7af531763
Iara Ferreira <ferreiraiara.10@gmail.com>
2025-08-15 08:59:39 -0300

    Validación de contraseña

--- registro/js/validacion.js
+++ registro/js/validacion.js
@@ -1,7 +1,20 @@
 function showAlertSuccess() {
-    document.getElementById("alert-success").classList.add("show");
+  document.getElementById("alert-success").classList.add("show");
 }
 
 function showAlertError() {
-    document.getElementById("alert-danger").classList.add("show");
+  document.getElementById("alert-danger").classList.add("show");
 }
+
+// Acá empieza la validación de contraseña
+
+document.getElementById("regBtn").addEventListener("click", function () {
+  let pass1 = document.getElementById("password1").value;
+  let pass2 = document.getElementById("password2").value;
+
+  if (pass1 === pass2) {
+    showAlertSuccess();
+  } else {
+    showAlertError();
+  }
+});
